db13e32b752191aaecd14c5a4570a95080453926
nhildred <aida_aida29@list.ru>
2022-08-04 22:28:04 +0300

    stepik/ex18

--- stepik/ex18/ex18.java
+++ stepik/ex18/ex18.java
@@ -6,12 +6,8 @@ public class ex18 {
 			l.setLevel(Level.ALL);
 			ConsoleHandler c = new ConsoleHandler();
 			XMLFormatter a = new XMLFormatter();
-			// типа в консоль запихиваем, т.е. устанавливаем формат выводимого на экран, т.е. мы          хотим
-			// видеть выводимое в виде ХМЛ
 			c.setFormatter(a);
-			// добовляем наш новообразованый с новыми настройками хэндлер.
 			l.addHandler(c);
-			//Выводим наши логи
 			c.setLevel(Level.ALL);
 			l.finest("Halyna");
 			l.info("My first log");
